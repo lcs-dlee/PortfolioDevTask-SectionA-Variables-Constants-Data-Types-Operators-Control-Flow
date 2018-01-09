cb966879697db1cb1dea1d8f3d56d9b1538f4410
Russell Gordon <rgordon@lcs.on.ca>
2018-01-09 08:13:26 -0500

    Solution to question 5

--- Portfolio Development Task.playground/Pages/Question 5.xcplaygroundpage/Contents.swift
+++ Portfolio Development Task.playground/Pages/Question 5.xcplaygroundpage/Contents.swift
@@ -15,8 +15,18 @@ let isMale = true
 let neverEverSarcastic = true
 
 // Answer question 5 below
+if teachesPhysics && isMale && !neverEverSarcastic {
+    print("Must be Braeckman!")
+} else {
+    print("Hmm, need more data to be sure.")
+}
 
-
+// Alternate solution, different syntax
+if teachesPhysics == true && isMale == true && neverEverSarcastic == false {
+    print("Must be Braeckman!")
+} else {
+    print("Hmm, need more data to be sure.")
+}
 /*:
  ## Now share your understanding
  
